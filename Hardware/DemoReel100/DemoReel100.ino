68ea4b7fde82bcb975ab547f0665a739d6372aa4
sam-dreams-of-buildings <sam.dreams.of.buildings@gmail.com>
2019-11-13 17:01:35 -0500

    white confetti slow

--- Hardware/DemoReel100/DemoReel100.ino
+++ Hardware/DemoReel100/DemoReel100.ino
@@ -120,7 +120,15 @@ void white_confetti()
   // random white speckles that blink in and fade smoothly
   fadeToBlackBy( leds, NUM_LEDS, 10);
   int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( 55, 200, 255);
+  leds[pos] += CHSV( 60, 200, 255);
+}
+
+void white_confetti_slow() 
+{
+  // random white speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 100);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( 60, 200, 255);
 }
 
 void juggle() {
